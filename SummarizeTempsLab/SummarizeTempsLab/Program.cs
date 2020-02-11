c1ef77eab28cfcee6f6c2cfb3e7a2a55fbb70244
SimonOtte99 <59981822+SimonOtte99@users.noreply.github.com>
2020-02-11 16:36:25 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,7 +7,61 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
+            string filename;
+            Console.WriteLine("Enter the filename");
             // temperature data is in temps.txt
+            filename = Console.ReadLine();
+
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("The file exists");
+
+                Console.WriteLine("Enter Threshold");
+                string input;
+                int threshold;
+                input = Console.ReadLine();
+                threshold = int.Parse(input);
+
+                int sumTemps = 0;
+                int numberbelow = 0;
+                int numberabove = 0;
+                int tempcount = 0;
+
+                using (StreamReader sr = File.OpenText(filename))
+                {
+                    string line = sr.ReadLine();
+                    int temp;
+                    while (line != null)
+                    {
+                        temp = int.Parse(line);
+                        sumTemps += temp;
+                        tempcount += 1;
+
+
+                        if (temp >= threshold)
+                        {
+                            numberabove += 1;
+
+                        }
+                        else
+                        {
+                            numberbelow += 1;
+
+                        }
+                        line = sr.ReadLine();
+                    }
+
+
+                    
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist");     
+            }
+                
         }
     }
 }
